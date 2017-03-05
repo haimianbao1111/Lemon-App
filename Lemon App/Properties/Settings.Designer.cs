52067d96ea957252f065977f74604f0cf252abed
TwilightLemon <cz241126@live.com>
2017-03-05 11:46:25 +0800

    V3

--- Lemon App/Properties/Settings.Designer.cs
+++ Lemon App/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Lemon_App.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.0.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -212,18 +212,6 @@ namespace Lemon_App.Properties {
             }
             set {
                 this["sx"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("么么哒")]
-        public string title {
-            get {
-                return ((string)(this["title"]));
-            }
-            set {
-                this["title"] = value;
             }
         }
     }
