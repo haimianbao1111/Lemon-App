5e74e94d6132e6ca1dde1670ad8206805bc17330
TwilightLemon <cz241126@live.com>
2017-03-06 20:55:11 +0800

    V3

--- Lemon App/UserFeedback.xaml.cs
+++ Lemon App/UserFeedback.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,34 +27,41 @@ namespace Lemon_App
         {
             InitializeComponent();
         }
-
+        private bool IsValidEmail(string strIn)
+        {
+            return Regex.IsMatch(strIn, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)" + @"|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+        }
         private void textBlock1_MouseDown(object sender, MouseButtonEventArgs e)
         {
             try
             {
-                MailMessage m = new MailMessage();
-                m.From = new MailAddress("lemon.app@qq.com","小萌反馈");
-                m.To.Add(new MailAddress("cz241126@live.com"));
-                m.Subject = "Lemon App用户反馈";
-                m.SubjectEncoding = Encoding.UTF8;
-                string body = "";
-                if (r.IsChecked == true)
-                    body = "喜欢";
-                else if (rr.IsChecked == true)
-                    body = "建议";
-                else if (rrr.IsChecked == true)
-                    body = "不喜欢";
-                He.EmailUFMsg.Replace("{ninini}", body).Replace("{nihaoyouxiang}", textBox1.Text).Replace("{lalalala}", textBox.Text);
-                m.Body = He.EmailUFMsg.Replace("{ninini}", body).Replace("{nihaoyouxiang}", textBox1.Text).Replace("{lalalala}", textBox.Text);
-                m.BodyEncoding = Encoding.UTF8;
-                m.IsBodyHtml = true;
-                SmtpClient s = new SmtpClient();
-                s.Host = "smtp.qq.com";
-                s.Port = 587;
-                s.EnableSsl = true;
-                s.Credentials = new NetworkCredential("lemon.app@qq.com", "qtmiqibczofmddbi");
-                s.Send(m);
-                label.Content = "成功发送你的反馈！";
+                if (textBox1.Text != string.Empty && IsValidEmail(textBox1.Text)&&textBox.Text!=string.Empty)
+                {
+                    MailMessage m = new MailMessage();
+                    m.From = new MailAddress("lemon.app@qq.com", "小萌反馈");
+                    m.To.Add(new MailAddress("cz241126@live.com"));
+                    m.Subject = "Lemon App用户反馈";
+                    m.SubjectEncoding = Encoding.UTF8;
+                    string body = "";
+                    if (r.IsChecked == true)
+                        body = "喜欢";
+                    else if (rr.IsChecked == true)
+                        body = "建议";
+                    else if (rrr.IsChecked == true)
+                        body = "不喜欢";
+                    He.EmailUFMsg.Replace("{ninini}", body).Replace("{nihaoyouxiang}", textBox1.Text).Replace("{lalalala}", textBox.Text);
+                    m.Body = He.EmailUFMsg.Replace("{ninini}", body).Replace("{nihaoyouxiang}", textBox1.Text).Replace("{lalalala}", textBox.Text);
+                    m.BodyEncoding = Encoding.UTF8;
+                    m.IsBodyHtml = true;
+                    SmtpClient s = new SmtpClient();
+                    s.Host = "smtp.qq.com";
+                    s.Port = 587;
+                    s.EnableSsl = true;
+                    s.Credentials = new NetworkCredential("lemon.app@qq.com", "qtmiqibczofmddbi");
+                    s.Send(m);
+                    label.Content = "成功发送你的反馈！";
+                }
+                else { label.Content = "请输入合法的Email地址"; }
             }
             catch (Exception ex)
             {
