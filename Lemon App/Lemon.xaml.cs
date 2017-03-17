a589473d94cac2f63337aa48a2fb61c63f81d4e8
TwilightLemon <cz241126@live.com>
2017-03-17 22:15:09 +0800

    V3 RTM

--- Lemon App/Lemon.xaml.cs
+++ Lemon App/Lemon.xaml.cs
@@ -25,12 +25,39 @@ namespace Lemon_App {
         {
             InitializeComponent();
             this.FontFamily = new FontFamily(Settings.Default.FontFamilly);
-            this.SourceInitialized += new EventHandler(win_SourceInitialized);
             t.Tick += T_Elapsed;
             t.Interval = 5000;
             t.Start();
         }
-    private void T_Elapsed(object sender, EventArgs e)
+        Rect rcnormal;
+        bool ismax = false;
+        /// <summary>
+        /// 最大化
+        /// </summary>
+        private void MaxEX()
+        {
+            BorderThickness = new Thickness(0);
+            Page.Clip = new RectangleGeometry() { RadiusX = 0, RadiusY = 0, Rect = new Rect() { Width = Page.ActualWidth, Height = Page.ActualHeight } };
+            rcnormal = new Rect(this.Left, this.Top, this.Width, this.Height);
+            this.Left = 0;
+            this.Top = 0;
+            Rect rc = SystemParameters.WorkArea;
+            this.Width = rc.Width;
+            this.Height = rc.Height;
+        }
+        /// <summary>
+        /// 还原
+        /// </summary>
+        private void EX()
+        {
+            BorderThickness = new Thickness(10);
+            Page.Clip = new RectangleGeometry() { RadiusX = 3, RadiusY = 3, Rect = new Rect() { Width = Page.ActualWidth, Height = Page.ActualHeight } };
+            this.Left = rcnormal.Left;
+            this.Top = rcnormal.Top;
+            this.Width = rcnormal.Width;
+            this.Height = rcnormal.Height;
+        }
+        private void T_Elapsed(object sender, EventArgs e)
         {
             GC.Collect();
             if (System.IO.File.Exists(Settings.Default.UserImage))
@@ -49,6 +76,11 @@ namespace Lemon_App {
         }
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
+            if (this.ActualHeight > SystemParameters.WorkArea.Height || this.ActualWidth > SystemParameters.WorkArea.Width)
+            {
+                this.WindowState = System.Windows.WindowState.Normal;
+                MaxEX();
+            }
             Page.Clip = new RectangleGeometry() { RadiusX = 5, RadiusY = 5, Rect = new Rect() { Width = Page.ActualWidth, Height = Page.ActualHeight } };
             //    (IContentPage.Child as UserControl).Width = IContentPage.ActualWidth;
             // (IContentPage.Child as UserControl).Height = ActualHeight;
@@ -171,10 +203,13 @@ namespace Lemon_App {
 
         private void MIN_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (this.WindowState == WindowState.Normal)
-            { BorderThickness = new Thickness(0); this.WindowState = WindowState.Maximized; Page.Clip = new RectangleGeometry() { RadiusX = 0, RadiusY = 0, Rect = new Rect() { Width = Page.ActualWidth, Height = Page.ActualHeight } }; }
-            else { BorderThickness = new Thickness(10); this.WindowState = WindowState.Normal; Page.Clip = new RectangleGeometry() { RadiusX = 3, RadiusY = 3, Rect = new Rect() { Width = Page.ActualWidth, Height = Page.ActualHeight } }; }
-        }
+           if (ismax)
+            {
+                ismax = !ismax;
+                EX();
+            }
+            else { MaxEX(); ismax = !ismax; }
+            }
 
         private void MAX_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -185,219 +220,6 @@ namespace Lemon_App {
         {
             new WelcomeWindow().Show();
         }
-        #region 最大化显示任务栏
-
-        void win_SourceInitialized(object sender, EventArgs e)
-        {
-            IntPtr handle = (new WindowInteropHelper(this)).Handle;
-            HwndSource.FromHwnd(handle).AddHook(new HwndSourceHook(WindowProc));
-        }
-
-        private  System.IntPtr WindowProc(
-              System.IntPtr hwnd,
-              int msg,
-              System.IntPtr wParam,
-              System.IntPtr lParam,
-              ref bool handled)
-        {
-            switch (msg)
-            {
-                case 0x0024:
-                    WmGetMinMaxInfo(hwnd, lParam);
-                    handled = true;
-                    break;
-            }
-
-            return (System.IntPtr)0;
-        }
-
-        private void WmGetMinMaxInfo(System.IntPtr hwnd, System.IntPtr lParam)
-        {
-
-            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
-            int MONITOR_DEFAULTTONEAREST = 0x00000002;
-            System.IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
-
-            if (monitor != System.IntPtr.Zero)
-            {
-
-                MONITORINFO monitorInfo = new MONITORINFO();
-                GetMonitorInfo(monitor, monitorInfo);
-                RECT rcWorkArea = monitorInfo.rcWork;
-                RECT rcMonitorArea = monitorInfo.rcMonitor;
-                mmi.ptMaxPosition.x = Math.Abs(rcWorkArea.left - rcMonitorArea.left);
-                mmi.ptMaxPosition.y = Math.Abs(rcWorkArea.top - rcMonitorArea.top);
-                mmi.ptMaxSize.x = Math.Abs(rcWorkArea.right - rcWorkArea.left);
-                mmi.ptMaxSize.y = Math.Abs(rcWorkArea.bottom - rcWorkArea.top);
-                mmi.ptMinTrackSize.x = Math.Abs((int)MinWidth);
-                mmi.ptMinTrackSize.y = Math.Abs((int)MinHeight);
-            }
-
-            Marshal.StructureToPtr(mmi, lParam, true);
-        }
-
-
-        /// <summary>
-        /// POINT aka POINTAPI
-        /// </summary>
-        [StructLayout(LayoutKind.Sequential)]
-        public struct POINT
-        {
-            /// <summary>
-            /// x coordinate of point.
-            /// </summary>
-            public int x;
-            /// <summary>
-            /// y coordinate of point.
-            /// </summary>
-            public int y;
-
-            /// <summary>
-            /// Construct a point of coordinates (x,y).
-            /// </summary>
-            public POINT(int x, int y)
-            {
-                this.x = x;
-                this.y = y;
-            }
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct MINMAXINFO
-        {
-            public POINT ptReserved;
-            public POINT ptMaxSize;
-            public POINT ptMaxPosition;
-            public POINT ptMinTrackSize;
-            public POINT ptMaxTrackSize;
-        };
-
-
-
-        /// <summary>
-        /// </summary>
-        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
-        public class MONITORINFO
-        {
-            /// <summary>
-            /// </summary>            
-            public int cbSize = Marshal.SizeOf(typeof(MONITORINFO));
-
-            /// <summary>
-            /// </summary>            
-            public RECT rcMonitor = new RECT();
-
-            /// <summary>
-            /// </summary>            
-            public RECT rcWork = new RECT();
-
-            /// <summary>
-            /// </summary>            
-            public int dwFlags = 0;
-        }
-
-
-        /// <summary> Win32 </summary>
-        [StructLayout(LayoutKind.Sequential, Pack = 0)]
-        public struct RECT
-        {
-            /// <summary> Win32 </summary>
-            public int left;
-            /// <summary> Win32 </summary>
-            public int top;
-            /// <summary> Win32 </summary>
-            public int right;
-            /// <summary> Win32 </summary>
-            public int bottom;
-
-            /// <summary> Win32 </summary>
-            public static readonly RECT Empty = new RECT();
-
-            /// <summary> Win32 </summary>
-            public int Width
-            {
-                get { return Math.Abs(right - left); }  // Abs needed for BIDI OS
-            }
-            /// <summary> Win32 </summary>
-            public int Height
-            {
-                get { return bottom - top; }
-            }
-
-            /// <summary> Win32 </summary>
-            public RECT(int left, int top, int right, int bottom)
-            {
-                this.left = left;
-                this.top = top;
-                this.right = right;
-                this.bottom = bottom;
-            }
-
-
-            /// <summary> Win32 </summary>
-            public RECT(RECT rcSrc)
-            {
-                this.left = rcSrc.left;
-                this.top = rcSrc.top;
-                this.right = rcSrc.right;
-                this.bottom = rcSrc.bottom;
-            }
-
-            /// <summary> Win32 </summary>
-            public bool IsEmpty
-            {
-                get
-                {
-                    // BUGBUG : On Bidi OS (hebrew arabic) left > right
-                    return left >= right || top >= bottom;
-                }
-            }
-            /// <summary> Return a user friendly representation of this struct </summary>
-            public override string ToString()
-            {
-                if (this == RECT.Empty) { return "RECT {Empty}"; }
-                return "RECT { left : " + left + " / top : " + top + " / right : " + right + " / bottom : " + bottom + " }";
-            }
-
-            /// <summary> Determine if 2 RECT are equal (deep compare) </summary>
-            public override bool Equals(object obj)
-            {
-                if (!(obj is Rect)) { return false; }
-                return (this == (RECT)obj);
-            }
-
-            /// <summary>Return the HashCode for this struct (not garanteed to be unique)</summary>
-            public override int GetHashCode()
-            {
-                return left.GetHashCode() + top.GetHashCode() + right.GetHashCode() + bottom.GetHashCode();
-            }
-
-
-            /// <summary> Determine if 2 RECT are equal (deep compare)</summary>
-            public static bool operator ==(RECT rect1, RECT rect2)
-            {
-                return (rect1.left == rect2.left && rect1.top == rect2.top && rect1.right == rect2.right && rect1.bottom == rect2.bottom);
-            }
-
-            /// <summary> Determine if 2 RECT are different(deep compare)</summary>
-            public static bool operator !=(RECT rect1, RECT rect2)
-            {
-                return !(rect1 == rect2);
-            }
-
-
-        }
-
-        [DllImport("user32")]
-        internal static extern bool GetMonitorInfo(IntPtr hMonitor, MONITORINFO lpmi);
-
-        /// <summary>
-        /// 
-        /// </summary>
-        [DllImport("User32")]
-        internal static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
-
-        #endregion
         private void Font_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             FontFamily = (Font.SelectedItem as ListBoxItem).FontFamily;
@@ -460,5 +282,20 @@ namespace Lemon_App {
             return IntPtr.Zero;
         }
         private const int WM_HOTKEY = 0x0312;
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (this.ActualWidth == SystemParameters.WorkArea.Width)
+                {
+                    EX();
+                }
+                else
+                {
+                    MaxEX();
+                }
+            }
+        }
     }
 }
