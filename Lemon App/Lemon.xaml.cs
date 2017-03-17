27c4125934374a8187649ae6ee4222e8d2221d74
TwilightLemon <cz241126@live.com>
2017-03-18 06:54:08 +0800

    V3 RTM

--- Lemon App/Lemon.xaml.cs
+++ Lemon App/Lemon.xaml.cs
@@ -25,21 +25,20 @@ namespace Lemon_App
             t.Interval = 5000;
             t.Start();
         }
-        Rect rcnormal;
         bool ismax = false;
         /// <summary>
         /// 最大化
         /// </summary>
         private void MaxEX()
         {
-            BorderThickness = new Thickness(0);
-            Page.Clip = new RectangleGeometry() { RadiusX = 0, RadiusY = 0, Rect = new Rect() { Width = Page.ActualWidth, Height = Page.ActualHeight } };
-            rcnormal = new Rect(this.Left, this.Top, this.Width, this.Height);
+            BorderThickness = new Thickness(15,15,0,0);
+            Page.Clip = new RectangleGeometry() { RadiusX = 0, RadiusY = 0, Rect = new Rect() { Width = ActualWidth, Height = ActualHeight } };
+            this.WindowState = WindowState.Maximized;
             this.Left = 0;
             this.Top = 0;
-            Rect rc = SystemParameters.WorkArea;
-            this.Width = rc.Width;
-            this.Height = rc.Height;
+    //        Rect rc = SystemParameters.WorkArea;
+    //        this.Width = rc.Width;
+     //       this.Height = rc.Height;
         }
         /// <summary>
         /// 还原
@@ -48,10 +47,11 @@ namespace Lemon_App
         {
             BorderThickness = new Thickness(10);
             Page.Clip = new RectangleGeometry() { RadiusX = 3, RadiusY = 3, Rect = new Rect() { Width = Page.ActualWidth, Height = Page.ActualHeight } };
-            this.Left = rcnormal.Left;
-            this.Top = rcnormal.Top;
-            this.Width = rcnormal.Width;
-            this.Height = rcnormal.Height;
+            this.WindowState = WindowState.Normal;
+            //this.Left = rcnormal.Left;
+            //this.Top = rcnormal.Top;
+            // this.Width = rcnormal.Width;
+            //   this.Height = rcnormal.Height;
         }
         private void T_Elapsed(object sender, EventArgs e)
         {
@@ -136,7 +136,9 @@ namespace Lemon_App
         }
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
-        {
+        {Rect rc = SystemParameters.WorkArea;
+            this.MaxWidth = rc.Width;
+            this.MaxHeight = rc.Height;
             IntPtr handle = new WindowInteropHelper(this).Handle;
             RegisterHotKey(handle, 124, (uint)4, (uint)System.Windows.Forms.Keys.Z);
             RegisterHotKey(handle, 125, (uint)4, (uint)System.Windows.Forms.Keys.S);
