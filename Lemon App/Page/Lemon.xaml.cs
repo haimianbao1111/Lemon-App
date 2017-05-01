9d72c833983792af75fb81835c7ce60afe09dd96
TwilightLemon <cz241126@live.com>
2017-05-01 18:16:08 +0800

    V3 RTM

--- Lemon App/Page/Lemon.xaml.cs
+++ Lemon App/Page/Lemon.xaml.cs
@@ -2,6 +2,7 @@
 using Lemon_App.Properties;
 using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
--- Lemon App/Page/LoadWindow.xaml.cs
+++ Lemon App/Page/LoadWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Lemon_App.Properties;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -17,6 +19,8 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using mshtml;
 
 namespace Lemon_App
 {
@@ -30,15 +34,77 @@ namespace Lemon_App
         public LoadWindow()
         {
             InitializeComponent();
+            wb.Navigate("http://ui.ptlogin2.qq.com/cgi-bin/login?appid=1006102&s_url=http://id.qq.com/index.html&hide_close_icon=1");
+            wb.Navigated += NaAsync;
             var c = new DoubleAnimation(0, 1, TimeSpan.FromSeconds(0.3));
             this.BeginAnimation(OpacityProperty, c);
             this.FontFamily = new FontFamily(Settings.Default.FontFamilly);
-            tr.Interval = 4000;
+            tr.Interval = 3000;
             tr.Tick += T;
-            trs.Interval = 4000;
+            trs.Interval = 3000;
             trs.Tick += Trs;
         }
+        int index = 0;
+        private async void NaAsync(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            if (index != 0)
+            {
+                if (wb.DocumentTitle == "我的QQ中心")
+                {
+                    var qq = He.Text(wb.Document.Cookie, "uin=o", ";", 0);
+                    wb.Dispose();
+                    var sl = He.Text(await Uuuhh.GetWebAsync("http://r.pengyou.com/fcg-bin/cgi_get_portrait.fcg?uins=" + qq, Encoding.Default), "portraitCallBack(", ")", 0);
+                    JObject o = JObject.Parse(sl);
+                    if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + qq + ".jpg"))
+                    {
+                        System.Net.WebClient x = new System.Net.WebClient();
+                        x.DownloadFileAsync(new Uri($"http://q2.qlogo.cn/headimg_dl?bs=qq&dst_uin={qq}&spec=100"), AppDomain.CurrentDomain.BaseDirectory + qq + ".jpg");
+                    }
+                    Settings.Default.RobotName = o[qq][6].ToString();
+                    Settings.Default.UserImage = AppDomain.CurrentDomain.BaseDirectory + qq + ".jpg";
+                    Settings.Default.LemonAreeunIts = qq + "@qq.com";
+                    Settings.Default.RNBM = (Boolean)RM.IsChecked;
+                    Settings.Default.Save();
+                    OS.Visibility = Visibility.Collapsed;
+                    RM.Visibility = Visibility.Collapsed;
+                    q.Visibility = Visibility.Collapsed;
+                    s.Visibility = Visibility.Collapsed;
+                    rk.Visibility = Visibility.Collapsed;
+                    ThicknessAnimationUsingKeyFrames t = new ThicknessAnimationUsingKeyFrames();
+                    t.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0, -45, 0, 0), TimeSpan.FromSeconds(0)));
+                    t.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0, 50, 0, 0), TimeSpan.FromSeconds(0.3)));
+                    t.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0, 50, 0, 0), TimeSpan.FromSeconds(3)));
+                    TX.BeginAnimation(MarginProperty, t);
+                    tr.Start();
+                }
+                else if (wb.DocumentText.Contains("安全验证"))
+                {
+                    op.IsOpen = true;
+                }
+                else { rk.Text = "登录失败,请检查账号和密码."; }
+            }else { index++; }
+        }
+        private System.Drawing.Image GetWebImage(System.Windows.Forms.WebBrowser WebCtl, HtmlElement ImgeTag)
+        {
+            HTMLDocument doc = (HTMLDocument)WebCtl.Document.DomDocument;
+            HTMLBody body = (HTMLBody)doc.body;
+            IHTMLControlRange rang = (IHTMLControlRange)body.createControlRange();
+            IHTMLControlElement Img = (IHTMLControlElement)ImgeTag.DomElement; //图片地址  
 
+            var oldImage = System.Windows.Forms.Clipboard.GetImage();
+            rang.add(Img);
+            rang.execCommand("Copy", false, null);  //拷贝到内存  
+            var numImage = System.Windows.Forms.Clipboard.GetImage();
+            try
+            {
+                System.Windows.Forms.Clipboard.SetImage(oldImage);
+            }
+            catch
+            {
+            }
+
+            return numImage;
+        }
         private void Trs(object sender, EventArgs e)
         {
             OS.Visibility = Visibility.Visible;
@@ -64,7 +130,8 @@ namespace Lemon_App
             }
             else
             {
-                Email.Text = Settings.Default.LemonAreeunIts;
+               var s = Settings.Default.LemonAreeunIts;
+                Email.Text = s.Remove(s.LastIndexOf("@qq.com"));
                 if (System.IO.File.Exists(Settings.Default.UserImage))
                 { TX.Background = new ImageBrush(new BitmapImage(new Uri(Settings.Default.UserImage, UriKind.Absolute))); }
                 RM.IsChecked = Settings.Default.RNBM;
@@ -139,7 +206,7 @@ namespace Lemon_App
                         Credentials = new NetworkCredential("lemon.app@qq.com", "qtmiqibczofmddbi")
                     };
                     s.Send(m);
-                    ns.Text = "发送成功";
+                //    ns.Text = "发送成功";
                     DoubleAnimationUsingKeyFrames d = new DoubleAnimationUsingKeyFrames();
                     d.KeyFrames.Add(new LinearDoubleKeyFrame(0, TimeSpan.FromSeconds(0)));
                     d.KeyFrames.Add(new LinearDoubleKeyFrame(1, TimeSpan.FromSeconds(0.3)));
@@ -149,7 +216,7 @@ namespace Lemon_App
                 }
             }catch(Exception es)
             {
-                ns.Text = "发送失败" + es.Message;
+       //        ns.Text = "发送失败" + es.Message;
                 DoubleAnimationUsingKeyFrames d = new DoubleAnimationUsingKeyFrames();
                 d.KeyFrames.Add(new LinearDoubleKeyFrame(0, TimeSpan.FromSeconds(0)));
                 d.KeyFrames.Add(new LinearDoubleKeyFrame(1, TimeSpan.FromSeconds(0.3)));
@@ -200,11 +267,11 @@ namespace Lemon_App
             e.Handled = re.IsMatch(e.Text);
         }
 
-        private void Email_KeyDown(object sender, KeyEventArgs e)
+        private void Email_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-                Border_MouseDown(null, null);
+                Border_MouseDown_1(null, null);
             }
          }
         private bool IsValidEmail(string strIn)
@@ -238,6 +305,60 @@ namespace Lemon_App
             Settings.Default.RNBM = (bool)RM.IsChecked;
             new QQLogin().Show();
             Close();
+        }
+     //   System.Windows.Forms.WebBrowser wb = new System.Windows.Forms.WebBrowser();
+        private async void Border_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if (Email.Text != string.Empty || PSW.Password != string.Empty)
+            {
+               
+                System.Windows.Forms.HtmlDocument doc = wb.Document;
+                doc.GetElementById("switcher_plogin").InvokeMember("click");
+                await Task.Delay(1000);
+                doc.GetElementById("u").InnerText = Email.Text;
+                await Task.Delay(1000);
+                doc.GetElementById("p").InnerText = PSW.Password;
+                await Task.Delay(1000);
+                doc.GetElementById("login_button").InvokeMember("click");
+            }
+        }
+
+        private async void Grid_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                op.IsOpen = false;
+                //    wb.Document..GetElementById("submit").InvokeMember("click");
+                if (wb.DocumentTitle == "我的QQ中心")
+                {
+                    var qq = He.Text(wb.Document.Cookie, "uin=o", ";", 0);
+                    wb.Dispose();
+                    var sl = He.Text(await Uuuhh.GetWebAsync("http://r.pengyou.com/fcg-bin/cgi_get_portrait.fcg?uins=" + qq, Encoding.Default), "portraitCallBack(", ")", 0);
+                    JObject o = JObject.Parse(sl);
+                    if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + qq + ".jpg"))
+                    {
+                        System.Net.WebClient x = new System.Net.WebClient();
+                        x.DownloadFileAsync(new Uri($"http://q2.qlogo.cn/headimg_dl?bs=qq&dst_uin={qq}&spec=100"), AppDomain.CurrentDomain.BaseDirectory + qq + ".jpg");
+                    }
+                    Settings.Default.RobotName = o[qq][6].ToString();
+                    Settings.Default.UserImage = AppDomain.CurrentDomain.BaseDirectory + qq + ".jpg";
+                    Settings.Default.LemonAreeunIts = qq + "@qq.com";
+                    Settings.Default.RNBM = (Boolean)RM.IsChecked;
+                    Settings.Default.Save();
+                    OS.Visibility = Visibility.Collapsed;
+                    RM.Visibility = Visibility.Collapsed;
+                    q.Visibility = Visibility.Collapsed;
+                    s.Visibility = Visibility.Collapsed;
+                    rk.Visibility = Visibility.Collapsed;
+                    ThicknessAnimationUsingKeyFrames t = new ThicknessAnimationUsingKeyFrames();
+                    t.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0, -45, 0, 0), TimeSpan.FromSeconds(0)));
+                    t.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0, 50, 0, 0), TimeSpan.FromSeconds(0.3)));
+                    t.KeyFrames.Add(new LinearThicknessKeyFrame(new Thickness(0, 50, 0, 0), TimeSpan.FromSeconds(3)));
+                    TX.BeginAnimation(MarginProperty, t);
+                    tr.Start();
+                }
+                else { rk.Text = "登录失败,请检查账号和密码."; }
+            }
         }
     }
 }
